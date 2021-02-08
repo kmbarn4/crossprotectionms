8ed7f244f056f2269ed79ddc415bdefc308a0d44
kmbarn4 <45067318+kmbarn4@users.noreply.github.com>
2021-02-08 15:17:11 -0500

    Add files via upload

---
+++ crossprotection_analyses_KMB.R
@@ -0,0 +1,46 @@
+# Author: KM Barnett
+# Last update: 8 Feb 2021
+
+#Data analysis for:
+#Asymmetric cross-strain protection in amphibian response to Batrachochytrium dendrobatidis vaccination paper
+
+
+### Survival analysis
+
+# Load data
+survival <- read.csv("/Users/kmbarnett/Documents/2020_fall/cross protection/data/xstrain_survival_analysis.csv")
+head(survival)
+
+library("KMsurv")
+library("survival")
+
+# Cox proportional hazards model
+model1 <- coxph(Surv(mortality_day,mortality_status)~vaccine_strain*livebd_strain, data=survival)
+summary(model1)
+
+### Infection intensity analysis
+
+# Load data
+fullexp = read.csv("/Users/kmbarnett/Documents/2020_fall/cross protection/data/kmb_load_data_for_stats copy.csv")
+
+library(glmmTMB)
+
+# Zero-inflated negative binomial model on Bd counts for 3x2 experimental design
+infection_intensity_model = glmmTMB(round(final.bd.load) ~ Vaccine*Live.Bd, family="nbinom2", ziformula = ~1, data = fullexp)
+summary(infection_intensity_model)
+
+# Post hoc test comparing ASW - Panama vs. California-Panama
+# data set used for these models only includes ASW- Panama and California-Panama
+ASW_v_CA = read.csv("/Users/kmbarnett/Documents/2021 winter/cross protection/cross protection stats/kmb_vaxstrain2.csv")
+
+
+m2 = glmmTMB(round(final_Bd) ~ Vaccine, family="nbinom2", ziformula = ~1, data=ASW_v_CA)
+summary(m2)
+
+# Post hoc test comparing Panama-Panama vs. California-Panama
+# data set used for these models only includes Panama-Panama and California-Panama only
+
+PA_v_CA = read.csv("/Users/kmbarnett/Documents/2021 winter/cross protection/cross protection stats/kmb_vaxstrain3.csv")
+
+m3 = glmmTMB(round(final_Bd) ~ Vaccine, family="nbinom2", ziformula = ~1, data=PA_v_CA)
+summary(m3)
